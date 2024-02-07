0ef1233fee11efef0bc612a257ec75fab0746511
evolonation <1838940019@qq.com>
2024-02-07 14:39:11 +0800

    move build.ninja

--- build_tools/build.py
+++ build_tools/build.py
@@ -1,8 +1,10 @@
 import hashlib
 import json
-from os import path
+import os.path as ospath
+import os
 import subprocess as sp
 import ninja_syntax as ninja
+import tempfile
 
 def compute_hash(file_path):
   # 以二进制读取文件内容
@@ -17,37 +19,62 @@ def compute_hash(file_path):
   # 返回哈希值的十六进制表示
   return sha256.hexdigest()
 
-build_dir = './build'
+root_dir = './'
 output_name = 'hello.exe'
-output_path = path.join(build_dir, 'out', output_name)
-obj_dir = path.join(build_dir, 'obj')
-pcm_dir = path.join(build_dir, 'pcm')
+
+build_dir = ospath.join(root_dir, 'build')
+os.makedirs(build_dir, exist_ok=True)
+ninja_build_file = ospath.join(build_dir, 'build.ninja')
+output_file = ospath.join(build_dir, 'out', output_name)
+obj_dir = ospath.join(build_dir, 'obj')
+pcm_dir = ospath.join(build_dir, 'pcm')
+ninja_file = ospath.join(build_dir, 'build.ninja')
+
 flags = ['C:/Users/18389/msys2/mingw64/bin/clang', '-std=c++23', '-fexperimental-library', '-nostdinc++', '-nostdlib++', '-isystem', 'C:/Users/18389/msys2/mingw64/include/c++/v1', '-Wno-unused-command-line-argument', '-Wno-reserved-module-identifier']
-compile_flags = flags + ['-I.\\third_party\include\\', f'-fprebuilt-module-path={pcm_dir}', '-c']
-precompile_flags = flags + ['-I.\\third_party\include\\', f'-fprebuilt-module-path={pcm_dir}', '--precompile']
-link_flags = flags + [ '-LC:/Users/18389/msys2/mingw64/lib', '-Lthird_party/static_library']
+include_dirs = [
+    ospath.join(root_dir, 'third_party\include')
+  ]
+include_flags = list(map(lambda x: '-I'+ospath.abspath(x), include_dirs))
+compile_flags = flags + include_flags +[f'-fprebuilt-module-path={ospath.abspath(pcm_dir)}', '-c']
+precompile_flags = flags + include_flags + [f'-fprebuilt-module-path={ospath.abspath(pcm_dir)}', '--precompile']
+
+link_dirs = [
+    ospath.join(root_dir, 'third_party\static_library')
+  ]
+link_flags = flags + [ '-LC:/Users/18389/msys2/mingw64/lib' ] + list(map(lambda x: '-L'+ospath.abspath(x), link_dirs))
 link_librarys = ['c++', 'vulkan-1', 'glfw3dll']
 
+# 所有文件路径都是相对于 root_dir 的规范化相对路径
 input_files = []
-with open('translation_units', 'rt') as f:
-  for line in f:
-    input_files.append(line.strip())
+
+dir_stack = [root_dir]
+while len(dir_stack) != 0:
+  dir = dir_stack.pop()
+  with open(ospath.join(dir, 'sources'), 'rt') as f:
+    for line in f:
+      path = ospath.join(dir, line.strip())
+      if ospath.isdir(path):
+        dir_stack.append(path)
+      else:
+        input_files.append(ospath.normpath(ospath.relpath(path, root_dir)))
 
 def exec_scan_deps(files):
-    compile_database_path = path.join(build_dir, 'commands_for_scan_dep.json')
-    compile_database = []
-    for file in files:
-      output = file + '.o'
-      command = compile_flags + [file, '-o', output, '-IC:/Users/18389/msys64/mingw64/lib/clang/17/include']
-      compile_database.append({
-          'file': file,
-          'directory': path.abspath('.'),
-          'arguments': command,
-          'output': output,
-        })
-    with open(compile_database_path, 'wt') as f:
+    with tempfile.NamedTemporaryFile('wt') as f:
+    # compile_database_path = ospath.join(build_dir, 'commands_for_scan_dep.json')
+      compile_database = []
+      for file in files:
+        output = file + '.o'
+        command = compile_flags + [file, '-o', output, '-IC:/Users/18389/msys64/mingw64/lib/clang/17/include']
+        compile_database.append({
+            'file': file,
+            'directory': ospath.abspath('.'),
+            'arguments': command,
+            'output': output,
+          })
+    # with open(compile_database_path, 'wt') as f:
       json.dump(compile_database, f, indent=4)
-    result = sp.run(f'clang-scan-deps -format=p1689 -compilation-database {compile_database_path}', stdout=sp.PIPE, check=True)
+      f.flush()
+      result = sp.run(f'clang-scan-deps -format=p1689 -compilation-database {f.name}', stdout=sp.PIPE, check=True)
     def get_info(rule):
       info = {
           'deps': list(map(lambda x: x['logical-name'], rule.get('requires', [])))
@@ -60,10 +87,10 @@ def exec_scan_deps(files):
 
 def get_scan_deps(files):
   
-  cache_path = path.join(build_dir, 'scan_deps.json')
+  cache_path = ospath.join(build_dir, 'scan_deps.json')
   
   file_hashes = {file: compute_hash(file) for file in files}
-  if path.exists(cache_path):
+  if ospath.exists(cache_path):
     with open(cache_path, 'rt') as f:
       scan_deps = json.load(f)
   else:
@@ -102,9 +129,9 @@ obj_files = []
 compile_commands = []
 
 def get_pcm_file(file):
-  return path.join(pcm_dir, path.relpath(dep_info[file]['module'].replace(':', '-') + '.pcm'))
+  return ospath.abspath(ospath.join(pcm_dir, ospath.relpath(dep_info[file]['module'].replace(':', '-') + '.pcm')))
 def get_object_file(file):
-  return path.join(obj_dir, path.relpath(file + '.o'))
+  return ospath.abspath(ospath.join(obj_dir, ospath.relpath(file + '.o')))
 
 rule_id = 0
 def generate_rule_name():
@@ -112,44 +139,45 @@ def generate_rule_name():
   rule_id += 1
   return f'rule_{rule_id}'
 
-with open('build.ninja', 'wt') as f:
+with open(ninja_build_file, 'wt') as f:
   ninja_writer = ninja.Writer(f)
   for file, info in dep_info.items():
+    abs_file = ospath.abspath(file)
     obj_file = get_object_file(file)
     obj_files.append(obj_file)
     dep_pcm_files = list(map(get_pcm_file, info['deps']))
 
     compile_command = {}
     if 'module' not in info:
-      command_obj = compile_flags + [file, '-o', obj_file]
+      command_obj = compile_flags + [abs_file, '-o', obj_file]
       rule_name = generate_rule_name()
-      ninja_writer.rule(rule_name, command_obj)
-      ninja_writer.build(outputs=[obj_file], rule=rule_name, inputs=dep_pcm_files+[file])
+      ninja_writer.rule(rule_name, command_obj, description=f'PRECOMPILE {ospath.relpath(obj_file, root_dir)}')
+      ninja_writer.build(outputs=[obj_file], rule=rule_name, inputs=dep_pcm_files+[abs_file])
       compile_command['arguments'] = command_obj
     else:
       pcm_file = get_pcm_file(file)
-      command_pcm = precompile_flags + [file, '-o', pcm_file]
+      command_pcm = precompile_flags + [abs_file, '-o', pcm_file]
       rule_name = generate_rule_name()
-      ninja_writer.rule(rule_name, command_pcm)
-      ninja_writer.build(outputs=[pcm_file], rule=rule_name, inputs=dep_pcm_files+[file])
+      ninja_writer.rule(rule_name, command_pcm, description=f'PRECOMPILE {ospath.relpath(pcm_file, root_dir)}')
+      ninja_writer.build(outputs=[pcm_file], rule=rule_name, inputs=dep_pcm_files+[abs_file])
 
       command_obj = compile_flags + [pcm_file, '-o', obj_file]
       rule_name = generate_rule_name()
-      ninja_writer.rule(rule_name, command_obj)
+      ninja_writer.rule(rule_name, command_obj, description=f'COMPILE {ospath.relpath(obj_file, root_dir)}')
       ninja_writer.build(outputs=[obj_file], rule=rule_name, inputs=dep_pcm_files+[pcm_file])
       ninja_writer.build(outputs=[dep_info[file]['module']], rule='phony', inputs=[obj_file])
     
       compile_command['arguments'] = command_pcm
 
     compile_command.update({
-        'directory': path.abspath(path.split(file)[0]),
-        'file': path.abspath(file),
+        'directory': ospath.abspath(ospath.split(file)[0]),
+        'file': ospath.abspath(file),
       })
     compile_commands.append(compile_command)
-  link_command = link_flags + ['-o', output_path] + obj_files + list(map(lambda x: '-l'+x, link_librarys))
+  link_command = link_flags + ['-o', output_file] + obj_files + list(map(lambda x: '-l'+x, link_librarys))
   rule_name = generate_rule_name()
-  ninja_writer.rule(rule_name, link_command)
-  ninja_writer.build(outputs=[output_path], rule=rule_name, inputs=obj_files)
+  ninja_writer.rule(rule_name, link_command, description=f'LINK {ospath.relpath(output_file)}')
+  ninja_writer.build(outputs=[output_file], rule=rule_name, inputs=obj_files)
 print('ninja generate done')
 
 with open('compile_commands.json', 'wt') as f:
