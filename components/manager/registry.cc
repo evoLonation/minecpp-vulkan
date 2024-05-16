57608834297208f76e18e72d6f00170196870337
anonymous5929 <anonymous5929>
2024-05-16 12:46:21 +0800

    1. update submanager 2. add DrawUnit and Camera

--- components/manager/registry.cc
+++ components/manager/registry.cc
@@ -1,7 +1,14 @@
 module manager;
 
 import manager.pipeline;
+import manager.draw_unit;
 
 void mng::Manager::registerSubManagers() {
-  _sub_managers.emplace_back(new SubManager<PipelineResource>{});
+  auto camera_manager = new SubManager<CameraResource>{};
+  auto pipeline_manager = new SubManager<PipelineResource>{};
+  auto draw_unit_manager = new SubManager<DrawUnitResource>{};
+  draw_unit_manager->registerRefManager(pipeline_manager);
+  _sub_managers.emplace_back(camera_manager);
+  _sub_managers.emplace_back(pipeline_manager);
+  _sub_managers.emplace_back(draw_unit_manager);
 }
