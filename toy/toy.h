4063dee1a53c12a3d5f9ca98b9264e71d01b3489
evolonation <1838940019@qq.com>
2024-10-30 23:47:20 +0800

    optimze log

--- toy/toy.h
+++ toy/toy.h
@@ -4,9 +4,14 @@
 #define __NAME_TUPLE_STRING(...)                                                                   \
   toy::macro::nameTupleFormat({ #__VA_ARGS__ }, toy::macro::getTuple(__VA_ARGS__))
 #define TOY_ASSERT(condition, ...)                                                                 \
-  toy::throwf(condition, "assert error: " #condition ",\n  {}", __NAME_TUPLE_STRING(__VA_ARGS__))
+  toy::throwf(                                                                                     \
+    static_cast<bool>(condition),                                                                  \
+    "assert error: {} ,\n  {}",                                                                    \
+    #condition,                                                                                    \
+    __NAME_TUPLE_STRING(__VA_ARGS__)                                                               \
+  )
 #define TOY_CHECK(condition, ...)                                                                  \
-  toy::checkf(condition, "check error: " #condition ",\n  {}", __NAME_TUPLE_STRING(__VA_ARGS__))
+  toy::checkf(condition, "check error: {} ,\n  {}", #condition, __NAME_TUPLE_STRING(__VA_ARGS__))
 
 #define TOY_DEBUG(...) toy::debug(__NAME_TUPLE_STRING(__VA_ARGS__))
 
