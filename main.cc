0ff63711edac034406a26aac6ae5fc578e73f541
evolonation <1838940019@qq.com>
2024-12-01 13:35:41 +0800

    change namespace cp to eg

--- main.cc
+++ main.cc
@@ -37,32 +37,32 @@ int main() {
     auto  ctx = ctx::Context{ "hello vulkan", 1920, 1080 };
     auto  dset_pools = rd::meta::DescriptorPools{};
     auto& input = fw::InputProcessor::getInstance();
-    auto  transform_gui = cp::ModelTransformGuiController{};
-    auto  render_pass = cp::LightPipeline{};
-    auto  click_observer = cp::ObjectClickObserver{};
-    // auto  move_manager = cp::MoveControllerManager{};
-    auto rotate_manager = cp::RotateControllerManager{};
+    auto  transform_gui = eg::ModelTransformGuiController{};
+    auto  render_pass = eg::LightPipeline{};
+    auto  click_observer = eg::ObjectClickObserver{};
+    // auto  move_manager = eg::MoveControllerManager{};
+    auto rotate_manager = eg::RotateControllerManager{};
 
-    auto camera = cp::Camera{};
-    auto controller = cp::CameraController{ &camera };
+    auto camera = eg::Camera{};
+    auto controller = eg::CameraController{ &camera };
     render_pass.setCamera(&camera.getData());
 
     auto [positions, normals, tex_coords, indices] = model::getModelInfo("model/viking_room.obj");
-    auto object1 = cp::SceneComponent{ {
-      cp::LightMesh{
+    auto object1 = eg::SceneComponent{ {
+      eg::LightMesh{
         std::move(positions), std::move(normals), std::move(tex_coords), std::move(indices) },
       rd::SampledTexture::fromFile(
         "model/viking_room.png", true, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT
       ),
     } };
-    auto cylinder = cp::SceneComponent{ cp::createShape(
-      cp::generateCylinder(1, 2, 77), glm::vec3{ 0.3, 0.5, 0.1 }, glm::vec3{ 0.0f, 0.0f, 1.0f }
+    auto cylinder = eg::SceneComponent{ eg::createShape(
+      eg::generateCylinder(1, 2, 77), glm::vec3{ 0.3, 0.5, 0.1 }, glm::vec3{ 0.0f, 0.0f, 1.0f }
     ) };
     cylinder.getTransform().location = { 0, 3, 0 };
-    auto cube = cp::SceneComponent{
-      cp::createShape(cp::generateCube(), glm::vec3{ 0.3, 0.5, 0.1 }, glm::vec3{ 0.0f, 0.0f, 1.0f })
+    auto cube = eg::SceneComponent{
+      eg::createShape(eg::generateCube(), glm::vec3{ 0.3, 0.5, 0.1 }, glm::vec3{ 0.0f, 0.0f, 1.0f })
     };
-    // auto selector_cube = cp::MoveTransformSelector{ &cube };
+    // auto selector_cube = eg::MoveTransformSelector{ &cube };
 
     // auto cylinder = std::make_unique<tool::SceneComponent>(tool::createShape(
     //   tool::generateCylinder(), glm::vec3{ 0.3, 0.5, 0.1 }, glm::vec3{ 0.0f, 0.0f, 10.0f }
@@ -73,13 +73,13 @@ int main() {
     // auto axis = std::make_unique<tool::SceneComponent>(tool::createShape(
     //   tool::generateAxis(), glm::vec3{ 0.3, 0.5, 0.1 }, glm::vec3{ 0.0f, 0.0f, 0.0f }
     // ));
-    auto move = cp::MoveController{};
+    auto move = eg::MoveController{};
 
-    // auto selector_cone = cp::MoveTransformSelector{};
-    auto cone = cp::SceneComponent{};
+    // auto selector_cone = eg::MoveTransformSelector{};
+    auto cone = eg::SceneComponent{};
     {
-      auto cone_ = cp::SceneComponent{ cp::createShape(
-        cp::generateCone(), glm::vec3{ 0.3, 0.5, 0.1 }, glm::vec3{ 0.0f, 0.0f, 5.0f }
+      auto cone_ = eg::SceneComponent{ eg::createShape(
+        eg::generateCone(), glm::vec3{ 0.3, 0.5, 0.1 }, glm::vec3{ 0.0f, 0.0f, 5.0f }
       ) };
       // selector_cone = { &cone_ };
       cone_.attachTo(&cube);
