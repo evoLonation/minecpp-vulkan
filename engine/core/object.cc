6c0fdf83420a05a5abe79e5ee4fe5e82e55b3805
evolonation <1838940019@qq.com>
2024-12-21 23:53:52 +0800

    move isSelect and FixedDistance to editor.transform

--- engine/core/object.cc
+++ engine/core/object.cc
@@ -4,41 +4,17 @@ module engine.object;
 
 namespace eg {
 
-SceneObject::~SceneObject() {
-  if (_draw_unit.get()) {
-    _draw_unit2object.erase(_draw_unit.get());
-  }
-}
-
 void SceneObject::setDrawUnit(uptr<DrawUnit> draw_unit) {
-  if (_draw_unit.get()) {
-    _draw_unit2object.erase(_draw_unit.get());
-  }
   _draw_unit = std::move(draw_unit);
   setActive(true);
-  if (_draw_unit.get()) {
-    _draw_unit2object[_draw_unit.get()] = this;
-  }
 }
 
 void SceneObject::setActive(bool active) {
   if (active) {
-    _draw_unit->setActive(&getTransToWorld());
+    _draw_unit->setActive(this, &getTransToWorld());
   } else {
     _draw_unit->resetActive();
   }
-}
-
-auto SceneObject::isSelected() -> bool {
-  return _draw_unit.get() && IdPipeline::getInstance().getSelectedDrawUnit() == _draw_unit.get();
-}
-
-auto SceneObject::getSelected() -> SceneObject* {
-  if (auto iter = _draw_unit2object.find(IdPipeline::getInstance().getSelectedDrawUnit());
-      iter != _draw_unit2object.end()) {
-    return iter->second;
-  }
-  return nullptr;
 }
 
 auto SceneObject::assetSerialize() -> AssetPackager {
@@ -68,26 +44,6 @@ void SceneObject::assetDeserialize(AssetUnpacker unpacker) {
       std::make_unique<DrawUnit>(mesh_ref.consume<Mesh>(), texture_ref.consume<Texture>(), up_layer)
     );
   }
-}
-
-FixedDistanceNode::FixedDistanceNode() {
-  _fixed_distance_node = std::make_shared<Node>();
-  _view_pos = &Camera::getInstance().getViewPos();
-  // todo: make _distance reactive too
-  _fixed_distance_node->attachTo(this);
-  _fixed_distance_node->getTransToParent().bindNow(
-    [this](glm::mat4 const& to_world, glm::vec3 const& location_camera) {
-      {
-        auto location_object = mt::transformDot(to_world, glm::vec3{ 0, 0, 0 });
-        auto new_location_object =
-          location_camera + _distance * glm::normalize(location_object - location_camera);
-        return mt::inverse(to_world) *
-               mt::translate(glm::vec3{ new_location_object - location_object }) * to_world;
-      }
-    },
-    &getTransToWorld(),
-    _view_pos
-  );
 }
 
 } // namespace eg
