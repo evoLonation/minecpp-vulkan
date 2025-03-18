6335e36c5a5256e6252db0cb8dfa2409090f8736
evolonation <1838940019@qq.com>
2025-03-18 17:14:35 +0800

    follow last commit

--- engine/engine.h
+++ engine/engine.h
@@ -1,10 +1,5 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-#define REGISTER_ASSET(cls)                                                                        \
-  static inline bool __asset_default_getter_register = []() {                                      \
-    ::eg::registerDefaultAsset<cls>();                                                             \
-    return true;                                                                                   \
-  }()
 
 #endif // ENGINE_H
